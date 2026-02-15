da9ba5ff359d763885368fde666c008e90602a08
4what <cheserega174@gmail.com>
2026-02-15 22:00:21 +0500

    Add files via upload

--- src/js/main.js
+++ src/js/main.js
@@ -3,3 +3,10 @@ import '../scss/styles.scss'
 
 // Импортируйте весь JS Bootstrap
 import * as bootstrap from 'bootstrap'
+
+const myModal = document.getElementById('myModal')
+const myInput = document.getElementById('myInput')
+
+myModal.addEventListener('shown.bs.modal', () => {
+  myInput.focus()
+})
